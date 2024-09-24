a50b644e27e18c921b77c45d98bd5b191b20c5d5
Douglas McMurtry <douglas.mcmurtry@pwc.com>
2024-09-24 14:07:54 -0400

    hotel flight and confirmation implemented

--- src/DurableFunctionOrchestration/Activities/HotelFunctions.cs
+++ src/DurableFunctionOrchestration/Activities/HotelFunctions.cs
@@ -1,5 +1,4 @@
-﻿using Azure.Core;
-using DurableFunctionOrchestration.Models;
+﻿using DurableFunctionOrchestration.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Text;
@@ -17,7 +16,7 @@ namespace DurableFunctionOrchestration.Activities
         }
 
         [Function(nameof(RegistrationAsync))]
-        public async Task<bool> RegistrationAsync([ActivityTrigger] string userId, FunctionContext executionContext)
+        public async Task<HotelReservationRequest> RegistrationAsync([ActivityTrigger] string userId, FunctionContext executionContext)
         {
             ILogger logger = executionContext.GetLogger(nameof(RegistrationAsync));
             logger.LogInformation("Creating hotel registration for user {userId}.", userId);
@@ -30,12 +29,12 @@ namespace DurableFunctionOrchestration.Activities
             if (!response.IsSuccessStatusCode)
             {
                 logger.LogError("Failed to create hotel registration for user {userId}.", userId);
-                return false;
+                return new();
             }
 
             logger.LogInformation("Hotel registration created for user {userId}.", userId);
 
-            return true;
+            return request;
         }
 
         private HotelReservationRequest GetReservationRequest()
@@ -45,8 +44,8 @@ namespace DurableFunctionOrchestration.Activities
             var reservationRequest = new HotelReservationRequest
             {
                 Id = Guid.NewGuid().ToString(),
-                Name = "John Doe",
-                CheckIn = DateTime.Now.AddDays(random.Next(1, 30)),
+                Name = "John Doe Hotel",
+                CheckIn = DateTime.Now,
                 CheckOut = DateTime.Now.AddDays(random.Next(31, 60)),
                 Address = "123 Main St, Redmond, WA 98052",
 
